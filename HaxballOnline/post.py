87025fd930906bc211ced3dad3b092c1db931e00
Qubaef <qubaefg@gmail.com>
2020-03-11 09:28:40 +0100

    Post update

--- HaxballOnline/post.py
+++ HaxballOnline/post.py
@@ -1,13 +1,10 @@
-from HaxballOnline.CirclePhysical import CirclePhysical
+from CirclePhysical import CirclePhysical
 
+class Player(CirclePhysical):
 
-class Post(CirclePhysical):
-    noChange = 0
     def __init__(self, game, px, py):
         super().__init__(game, px, py, 0, 1, 15, (0,0,0))
-        self.noChange = 1
         self.to_sector_add()
 
-    def __setattr__(self, key, value):
-        if(self.noChange == 0):
-            super(Post, self).__setattr__(key, value)
+    def update(self):
+        return 1
