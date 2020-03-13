4f4e77f8b74fe143a42a8cf417b98a115839c46f
barska-ola@tlen.pl <barska-ola@tlen.pl>
2020-03-13 19:36:49 +0100

    Goal.h - finish Goal.cpp - finish

--- Serwer/SerwerApplication/Goal.cpp
+++ Serwer/SerwerApplication/Goal.cpp
@@ -1,1 +1,42 @@
 #include "Goal.h"
+#include "CirclePhysical.h"
+
+Goal::Goal(GameEngine* pGame, int x, int yUp, int yDown, int width, int direction)
+{
+	this->pGame = pGame;
+	this->x = x;
+	this->yUp = yUp;
+	this->yDown = yDown;
+	this->width = width;
+	this->direction = direction;
+	this->pPostDown = pPostDown;
+	this->pPostUp = pPostUp;
+	this->pPostUp = new Post(pGame,x,yUp);
+	this->pPostDown = new Post(pGame, x, yDown);
+}
+
+int Goal::getPx()
+{
+	return this->x;
+}
+
+int Goal::getPy()
+{
+	return this->yUp;
+}
+
+int Goal::getWidth()
+{
+	return this->width;
+}
+
+int Goal::getHeight()
+{
+	return this->yDown-this->yUp;
+}
+
+void Goal::goalCollide()
+{
+	this->pPostDown->collide();
+	this->pPostUp->collide();
+}
--- Serwer/SerwerApplication/Goal.h
+++ Serwer/SerwerApplication/Goal.h
@@ -12,7 +12,11 @@ private:
 	Post *pPostUp;
 	Post *pPostDown;
 public:
-	Goal(GameEngine* pGame, int x, int yUp, int yDown, int width, int direction, Post* pPostUp, Post* pPostDown);
-	
+	Goal(GameEngine* pGame, int x, int yUp, int yDown, int width, int direction);
+	int getPx();
+	int getPy();
+	int getWidth();
+	int getHeight();
+	void goalCollide();
 };
 
