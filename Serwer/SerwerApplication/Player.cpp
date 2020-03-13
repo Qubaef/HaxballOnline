50521d5980469695a228edb07ce0206144c432d7
barska-ola@tlen.pl <barska-ola@tlen.pl>
2020-03-13 21:14:34 +0100

    fixing player.cpp

--- Serwer/SerwerApplication/Player.cpp
+++ Serwer/SerwerApplication/Player.cpp
@@ -15,10 +15,11 @@ void Player::kick(Vector2D pos)
 	Ball* b = this->pGame->getBall();
 	Vector2D v = pos-b->getMove();
 
-	float dist = (this->getPosition->getX() - b->getPosition->getX()) ^ 2;
+
+	float dist = (this->getPosition().getX() - b->getPosition().getX()) ^ 2;
 	dist += (b->getPosition->getY() - this->getPosition->getY())^2;
 
-	if (dist <= (this->pGame->getBall.getHitbox() + this->getHitbox()) ^ 2)
+	if (dist <= (b->getHitbox() + this->getHitbox()) ^ 2)
 	{
 		//kick ball to given position
 		v=v.Normal(v)*v.length()/12;
@@ -30,13 +31,13 @@ void Player::modeBallControl()
 {
 	//turn down ball_control to reduce the bounce
 	this->ballControl = 0.1;
-	this->vMax = 2 / pow(this->getWeight,2/3);
+	this->vMax = 2 / pow(this->getWeight(),2/3);
 }
 
 void Player::modeNormal()
 {
 	// bring back normal mode
 	this->ballControl = 1.0;
-	this->vMax = 6 / pow(this->getWeight, 2 / 3);
+	this->vMax = 6 / pow(this->getWeight(), 2 / 3);
 }
 
