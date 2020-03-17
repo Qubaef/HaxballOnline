a46a445aa797b5edbb88e0bd532dec547ccf8f92
Dominik Kwiatowski <domik.rex@gmail.com>
2020-03-17 18:09:57 +0100

    test

--- Serwer/SerwerApplication/main.cpp
+++ Serwer/SerwerApplication/main.cpp
@@ -5,10 +5,21 @@ int main()
 {
 	GameEngine game = GameEngine();
 
-	while(true)
+	for(int i=0;i<255;i++)
 	{
 		game.redraw();
-		game.getBall()->setMove(Vector2D(-1, 0));
+		if(i%10==0)
+			game.getBall()->setMove(Vector2D(3, 3));
+		if (i == 50)
+			int a = 0;
+		if (i == 100)
+			int a = 0;
+		if (i == 125)
+			int a = 0;
+		if (i == 150)
+			int a = 0;
+		if (i == 200)
+			int a = 0;
 	}
 
 	return 1;
