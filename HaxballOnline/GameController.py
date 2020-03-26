af81a8aabb41e396a37cde321d83d80223d20989
Jakub Fedorowicz <qubaefg@gmail.com>
2020-03-26 16:47:52 +0100

    fix

--- HaxballOnline/GameController.py
+++ HaxballOnline/GameController.py
@@ -14,10 +14,11 @@ from Ball import Ball
 HOST = '127.0.0.1'
 PORT = 8080 
 
-while (true):
+while (True):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b'Hello, client here!' + chr(0))
+        message = 'Hello, client here!' + chr(0)
+        s.sendall(bytes(message, encoding='utf-8'))
         data = s.recv(1024)
         print(data)
 
